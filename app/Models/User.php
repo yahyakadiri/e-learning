d945daee983527a7df45215fd1891e3b90f93740
Yahya El kadiri <yahya.e@flowdesignssolutions.com>
2022-08-31 15:43:06 +0100

    Revert "user model comment"
    This reverts commit 04d97877fdd9e1266f5b0511bbbc4ee6c0114a33.

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,6 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
 
-//User Model
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
