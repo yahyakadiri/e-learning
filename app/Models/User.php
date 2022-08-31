04d97877fdd9e1266f5b0511bbbc4ee6c0114a33
Yahya El kadiri <yahya.e@flowdesignssolutions.com>
2022-08-31 15:34:40 +0100

    user model comment

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
 
+//User Model
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
